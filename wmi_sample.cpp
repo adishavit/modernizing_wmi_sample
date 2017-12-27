f42b3772835bfe31950272b3f553048db0d663ae
Adi Shavit <adishavit@gmail.com>
2017-12-27 16:25:20 +0200

    Uses exceptions for error handling and the TRY macro for single line conditional return.

--- wmi_sample.cpp
+++ wmi_sample.cpp
@@ -10,47 +10,33 @@
 
 #include "scoped_utils.h"
 
+#define TRY(exp)      do { auto hr__ = exp; if (hr__ < 0) throw hr__; } while(0)
 
-int Win32_PerfRawData_PerfProc_Process()
+void Win32_PerfRawData_PerfProc_Process()
 {
-   // To add error checking,
-   // check returned HRESULT below where collected.
-   HRESULT hr = S_OK;
-
-   if (FAILED(hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED)))
-      return hr;
+   TRY(CoInitializeEx(nullptr, COINIT_MULTITHREADED));
    auto uniniter = make_scoped_invoker(CoUninitialize);
 
-   if (FAILED(hr = CoInitializeSecurity(nullptr, -1, nullptr, nullptr, RPC_C_AUTHN_LEVEL_NONE, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE, 0)))
-      return hr;
+   TRY(CoInitializeSecurity(nullptr, -1, nullptr, nullptr, RPC_C_AUTHN_LEVEL_NONE, RPC_C_IMP_LEVEL_IMPERSONATE, nullptr, EOAC_NONE, 0));
 
    auto pWbemLocator = [&]
    {
       IWbemLocator* pWbemLocator = nullptr;
-      if (FAILED(hr = CoCreateInstance(CLSID_WbemLocator, nullptr, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (void**)&pWbemLocator)))
-         return scoped_ptr<IWbemLocator>();
+      TRY(CoCreateInstance(CLSID_WbemLocator, nullptr, CLSCTX_INPROC_SERVER, IID_IWbemLocator, (void**)&pWbemLocator));
       return make_scoped_ptr(pWbemLocator, [](IWbemLocator* ptr) { ptr->Release(); });
    }();
-   if (!pWbemLocator)
-      return hr;
 
    // Connect to the desired namespace.
    auto bstrNameSpace = make_scoped_ptr(SysAllocString(L"\\\\.\\root\\cimv2"), ::SysFreeString);
    if (!bstrNameSpace)
-   {
-      hr = E_OUTOFMEMORY;
-      return hr;
-   }
+      throw E_OUTOFMEMORY;
 
    auto pNameSpace = [&]
    {
       IWbemServices* pNameSpace = nullptr;
-      if (FAILED(hr = pWbemLocator->ConnectServer(bstrNameSpace, nullptr, nullptr, nullptr, 0L, nullptr, nullptr, &pNameSpace)))
-         return scoped_ptr<IWbemServices>();
+      TRY(pWbemLocator->ConnectServer(bstrNameSpace, nullptr, nullptr, nullptr, 0L, nullptr, nullptr, &pNameSpace));
       return make_scoped_ptr(pNameSpace, [](IWbemServices* ptr) { ptr->Release(); });
    }();
-   if (!pNameSpace)
-      return hr;
 
    pWbemLocator.release();
    bstrNameSpace.release();
@@ -58,30 +44,23 @@ int Win32_PerfRawData_PerfProc_Process()
    auto pRefresher = [&]
    {
       IWbemRefresher*pRefresher = nullptr;
-      if (FAILED(hr = CoCreateInstance(CLSID_WbemRefresher, nullptr, CLSCTX_INPROC_SERVER, IID_IWbemRefresher, (void**)&pRefresher)))
-         return scoped_ptr<IWbemRefresher>();
+      TRY(CoCreateInstance(CLSID_WbemRefresher, nullptr, CLSCTX_INPROC_SERVER, IID_IWbemRefresher, (void**)&pRefresher));
       return make_scoped_ptr(pRefresher, [](IWbemRefresher* ptr) { ptr->Release(); });
    }();
-   if (!pRefresher)
-      return hr;
 
    auto pConfig = [&]
    {
       IWbemConfigureRefresher *pConfig = nullptr;
-      if (FAILED(hr = pRefresher->QueryInterface(IID_IWbemConfigureRefresher, (void **)&pConfig)))
-         return scoped_ptr<IWbemConfigureRefresher>();
+      TRY(pRefresher->QueryInterface(IID_IWbemConfigureRefresher, (void **)&pConfig));
       return make_scoped_ptr(pConfig, [](IWbemConfigureRefresher* ptr) { ptr->Release(); });
    }();
-   if (!pRefresher)
-      return hr;
 
    // Add an enumerator to the refresher.
    auto pEnum = [&]
    {
       IWbemHiPerfEnum *pEnum = nullptr;
       long lID = 0;
-      if (FAILED(hr = pConfig->AddEnum(pNameSpace, L"Win32_PerfRawData_PerfProc_Process", 0, nullptr, &pEnum, &lID)))
-         return scoped_ptr<IWbemHiPerfEnum>();
+      TRY(pConfig->AddEnum(pNameSpace, L"Win32_PerfRawData_PerfProc_Process", 0, nullptr, &pEnum, &lID));
       return make_scoped_ptr(pEnum, [](IWbemHiPerfEnum* ptr) { ptr->Release(); });
    }();
    pConfig.release();
@@ -102,66 +81,52 @@ int Win32_PerfRawData_PerfProc_Process()
 
    for (auto x = 0; x < 3; ++x)
    {
-      if (FAILED(hr = pRefresher->Refresh(0L)))
-         return hr;
+      TRY(pRefresher->Refresh(0L));
 
       DWORD dwNumReturned = 0;
-      hr = pEnum->GetObjects(0L, 0, nullptr, &dwNumReturned);
-      // If the buffer was not big enough,
-      // allocate a bigger buffer and retry.
+      auto hr = pEnum->GetObjects(0L, 0, nullptr, &dwNumReturned);      
       if (hr == WBEM_E_BUFFER_TOO_SMALL && dwNumReturned > apEnumAccess.size())
-      {
-         apEnumAccess.resize(dwNumReturned, nullptr);
-         if (FAILED(hr = pEnum->GetObjects(0L, (DWORD)apEnumAccess.size(), apEnumAccess.data(), &dwNumReturned)))
-            return hr;
+      {  
+         apEnumAccess.resize(dwNumReturned, nullptr);                                                    // If the buffer was not big enough, 
+         TRY(pEnum->GetObjects(0L, (DWORD)apEnumAccess.size(), apEnumAccess.data(), &dwNumReturned));  // allocate a bigger buffer and retry.
       }
-      else
-      {
-         if (hr == WBEM_S_NO_ERROR)
-         {
-            hr = WBEM_E_NOT_FOUND;
-            return hr;
-         }
-      }
+      else if (hr == WBEM_S_NO_ERROR)
+            throw WBEM_E_NOT_FOUND;
 
       // First time through, get the handles.
       if (0 == x)
       {
          CIMTYPE VirtualBytesType;
          CIMTYPE ProcessHandleType;
-         if (FAILED(hr = apEnumAccess[0]->GetPropertyHandle(L"VirtualBytes", &VirtualBytesType, &lVirtualBytesHandle)))
-            return hr;
-
-         if (FAILED(hr = apEnumAccess[0]->GetPropertyHandle(L"IDProcess", &ProcessHandleType, &lIDProcessHandle)))
-            return hr;
+         TRY(apEnumAccess[0]->GetPropertyHandle(L"VirtualBytes", &VirtualBytesType, &lVirtualBytesHandle));
+         TRY(apEnumAccess[0]->GetPropertyHandle(L"IDProcess", &ProcessHandleType, &lIDProcessHandle));
       }
 
       for (DWORD i = 0; i < dwNumReturned; i++)
       {
          DWORD dwVirtualBytes = 0;
-         if (FAILED(hr = apEnumAccess[i]->ReadDWORD(lVirtualBytesHandle, &dwVirtualBytes)))
-            return hr;
+         TRY(apEnumAccess[i]->ReadDWORD(lVirtualBytesHandle, &dwVirtualBytes));
 
          DWORD dwIDProcess = 0;
-         if (FAILED(hr = apEnumAccess[i]->ReadDWORD(lIDProcessHandle, &dwIDProcess)))
-            return hr;
+         TRY(apEnumAccess[i]->ReadDWORD(lIDProcessHandle, &dwIDProcess));
 
          wprintf(L"Process ID %lu is using %lu bytes\n",dwIDProcess, dwVirtualBytes);
       }
       
       Sleep(1000); // Sleep for a second.
    }
-
-   return 1;
 }
 
 int main()
 {
-   auto hr = Win32_PerfRawData_PerfProc_Process();
-   if (FAILED(hr))
+   try
+   {
+      Win32_PerfRawData_PerfProc_Process();
+      return EXIT_SUCCESS;
+   }
+   catch (HRESULT hr)
    {
       wprintf(L"Error status=%08x\n", hr);
       return EXIT_FAILURE;
    }
-   return EXIT_SUCCESS;
 }
